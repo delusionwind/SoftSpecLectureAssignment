3ce45e32bc4e2703238d93247b8c00b2e5e951cf
Delusion <delusionwind@gmail.com>
2017-03-13 15:33:26 +0700

    initialized git

---
+++ src/ui/Terminal.java
@@ -0,0 +1,51 @@
+package ui;
+
+import java.util.Scanner;
+import game.Game;
+
+public class Terminal {
+
+	private Game game;
+	private Scanner scanner;
+
+	public void run() {
+		game = new Game();
+		game.start();
+		scanner = new Scanner(System.in);
+		renderBoard(game);
+		while (!game.isEnd()) {
+			System.out.println(game.getCurrentPlayerName() + "'s turn");
+			
+			System.out.print("Row: ");
+			int row = scanner.nextInt();
+			System.out.print("Col: ");
+			int col = scanner.nextInt();
+			
+			game.currentPlayerTakesAction(row, col);
+			renderBoard(game);
+			if(game.getWinner() != null) {
+				System.out.println(game.getWinnerName() + " Win");
+			}
+		}
+	}
+
+	private void renderBoard(Game game) {
+		int size = game.getBoardSize();
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				String s = game.getSymbolOnBoard(row, col);
+				if(s == null) {
+					s = "_";
+				}
+				System.out.print(s);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Terminal ui = new Terminal();
+		ui.run();
+	}
+
+}
